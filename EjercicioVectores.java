3501bbb788c9667a4b477d3d14109b00c7735a15
daniel21connor <jconnorr1@miumg.edu.gt>
2022-05-14 13:28:34 +0200

    se agrego el ejercicio

---
+++ EjercicioVectores.java
@@ -0,0 +1,33 @@
+
+package ejerciciovectores;
+
+import java.util.Arrays;
+
+
+public class EjercicioVectores {
+public static void main(String[] args) {
+   int []cantidad=new int [100], aux;
+   //int suma=100;
+   for  (int i=0; i<100; i++){
+    cantidad[i]=i+1;
+        //System.out.println(""+aleat);
+        //cantidad[i]=aleat;
+        
+}
+   Arrays.sort(cantidad);
+for (int i=cantidad.length-1; i>=0; i--){
+   
+        
+    System.out.println(cantidad[i]);
+    }
+    
+    
+    
+    
+    
+    
+   
+}
+}   
+}
+  
